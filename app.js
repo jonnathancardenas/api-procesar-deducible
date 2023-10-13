320001590a758301c7a0017eafe48586b461e16d
Jonnathan Cardenas Perez <jonnathan.cardenasp@rimac.com.pe>
2023-10-13 01:54:43 -0500

    feat: API de procesamiento de deducibles

---
+++ app.js
@@ -0,0 +1,58 @@
+const express = require('express');
+const app = express();
+app.use(express.json());
+
+app.listen(8000);
+
+app.post('/procesar-deducible', (req, res) => {
+
+  const text = req.body.text;
+
+  const patroneDeducibles = /(\*|-)\s*(.*?)(?=\s*\*|\s*-|$)/g;
+
+  let encontrado;
+  const listaEncontrados = [];
+
+  while ((encontrado = patroneDeducibles.exec(text))) {
+    listaEncontrados.push(encontrado[2]);
+  }
+  const listaDeducibles = listaEncontrados.length > 0 ? listaEncontrados : [ text ];
+  const deducibles = listaDeducibles.map((deducible) => {
+    let patronDeducible = /(\d+(\.\d+)?)%\s*del monto a indemnizar/i.exec(deducible);
+    let valorDeducible = patronDeducible ? parseInt(patronDeducible[1]) : 0;
+    patronDeducible = /(\d+(\.\d+)?)%\s*del monto indemnizable/i.exec(deducible);
+    valorDeducible = patronDeducible ? parseInt(patronDeducible[1]) : valorDeducible;
+    patronDeducible = /(\d+(\.\d+)?)%\s*del monto del siniestro/i.exec(deducible);
+    valorDeducible = patronDeducible ? parseInt(patronDeducible[1]) : valorDeducible;
+
+    const patronCopago = /mínimo\s+(US\$|S\/)\s*(\d+(\.\d+)?)/i.exec(deducible);
+    const valorCopago = patronCopago ? parseInt(patronCopago[2]) : 0;
+
+    let moneda = patronCopago[1];
+    if (moneda === 'US$') {
+      moneda = 'USD';
+    } else if (moneda === 'S/') {
+      moneda = 'SOL';
+    }
+
+    const tipo = /MULTIMARCA/i.test(deducible.toUpperCase()) ? "Multimarca" : "NO TIPO";
+    const patronMarca = /Marca([^\n]*)\n/i.exec(deducible);
+    const marca = patronMarca ? patronMarca[1].trim() : "NO MARCA";
+    const patronTaller = /taller(es)? ([^\n]*)\n/i.exec(deducible);
+    const taller = patronTaller ? patronTaller[2] : "NO TALLER";
+
+    return {
+      deducible: valorDeducible,
+      copago: valorCopago,
+      moneda: moneda,
+      tipo: tipo,
+      marca: marca,
+      taller: taller
+    };
+  });
+
+  const resultado = {
+    payload: deducibles
+  };
+  res.json({requestBody: resultado})
+});
